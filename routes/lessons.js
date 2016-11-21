58ee0c7e140e5001681b1cb9b5232beaa302cc18
TTonyJ <609950770@qq.com>
2016-11-21 10:38:12 +0800

    支付接口更新

--- routes/lessons.js
+++ routes/lessons.js
@@ -211,9 +211,9 @@ router.post('/details', function(req, res) {
                 res.json({status:'error','errcode':2});
                 return;
             }else{
-                Bill.findOne({lesson:lessonID,student:userID},function(err,pay){
+                Bill.findOne({lesson:lessonID,student:userID},function(err,bill){
                     var paystate = "unpaid"; // 0
-                    if(!err && !!pay) {
+                    if(!err && bill.status==true) {
                         paystate = "paid";
                     }
                     if(lesson.price == 0){
